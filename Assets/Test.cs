2a2fea66fea7451aedc10faacb750835a269bd9b
TakahikoOgihara <TakahikoOgihara@users.noreply.github.com>
2022-05-23 20:15:02 +0900

    GitHubのPUSHのテスト
    TA（Unity）でGithubのステージングとコミットの課題提出のため。

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int score;
+        score = 150;
+        int score2 = 200;
+        int score3 = score + score2;
+        Debug.Log(score3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
